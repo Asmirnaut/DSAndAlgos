de7abd02be959f83f7412a1f758e0f7fb8900952
Ally S <ally.lobova@gmail.com>
2020-05-11 20:58:49 -0400

    tree traversal

--- DataStructures/TreeTraversal.js
+++ DataStructures/TreeTraversal.js
@@ -61,4 +61,51 @@ class BST {
     traverse(current);
     return visited;
   }
+
+  //Post Order DFS - recursive
+  //Create a variable to store the values of nodes visited
+  //Store the root of the BST ina variable called current
+  //Write a helper function which accepts a node
+  //If the node has a left property, call the helper function with the left property
+  //If the node has a right property, call the helper function with the right property on the node
+  //Push the values of the node to the variable that stores the values
+  //Invoke the helper function with the current variable
+  //Return the array of values
+
+  postOrder() {
+    let visited = [];
+    let current = this.root;
+
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      visited.push(node);
+    };
+
+    traverse(current);
+    return visited;
+  }
+
+  //InOrder DFS - recursive
+  //Create a variable to store visited nodes
+  //create a variable to store the root
+  //Write helper function that takes a node
+  //If the node has a left property, call helper function on left prop of node
+  //Push the node to the visited array
+  //If the node has a right property, call helper funciton on right property of the node
+  //Invoke helper function on root
+  //return visited array
+
+  inOrder() {
+    let visited = [];
+    let current = this.root;
+
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      visited.push(node);
+      if (node.right) traverse(node.right);
+    };
+    traverse(current);
+    return visited;
+  }
 }
