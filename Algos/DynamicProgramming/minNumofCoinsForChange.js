fa1bd8cbdc0a6f5ec537969573bb8746db2106d0
Ally S <ally.lobova@gmail.com>
2020-05-19 16:55:07 -0400

    leetcode 322

---
+++ Algos/DynamicProgramming/minNumofCoinsForChange.js
@@ -0,0 +1,29 @@
+/* You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1. */
+
+/*
+Example 1:
+
+Input: coins = [1, 2, 5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+
+
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+*/
+
+var coinChange = function (coins, amount) {
+  let numCoins = new Array(amount + 1).fill(Infinity);
+  numCoins[0] = 0;
+
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = 1; j < numCoins.length; j++) {
+      if (coins[i] <= j) {
+        numCoins[j] = Math.min(numCoins[j], 1 + numCoins[j - coins[i]]);
+      }
+    }
+  }
+  return numCoins[amount] !== Infinity ? numCoins[amount] : -1;
+};
